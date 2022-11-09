6376bf28908d8e8753599757c7ba20ed75862613
NarcizzeN <47037769+NarcizzeN@users.noreply.github.com>
2022-11-09 16:35:38 +0000

    emptied Head in _document.tsx added Head with viewport in _app.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { getInitColorSchemeScript } from '@mui/material/styles';
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { getInitColorSchemeScript } from "@mui/material/styles";
 
 export default class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <meta name="viewport" content="initial-scale=1, width=device-width" />
-                </Head>
-                <body>
-                {getInitColorSchemeScript()}
-                <Main />
-                <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html>
+          <Head />
+        <body>
+          {getInitColorSchemeScript()}
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
